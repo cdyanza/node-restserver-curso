e1d48cf4cf8a1b622da15fd42631dbf2daaf46e4
Cristian <yanzacd@gmail.com>
2018-07-31 11:02:36 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // =================================================
 process.env.PORT = process.env.PORT || 3000;
+
+// =================================================
+// Entorno
+// =================================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =================================================
+// Base fr datos
+// =================================================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:cafe123456@ds155727.mlab.com:55727/cafe';
+}
+
+process.env.URLDB = urlDB;
